ff88f783b8877659df55250c0edebf7353c87694
kelvin beno <kelvinbeno526@gmail.com>
2019-01-28 09:50:14 +0300

    modified templates and models set up

--- apps.py
+++ apps.py
@@ -1,29 +1,106 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6068758f589fd21f74dd31a4694faa13'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kelvin:5000@localhost/stackdatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String('12345'), nullable=False)
+    post_question = db.relationship('Post', lazy=True)
+    post_answer = db.relationship('Post', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+'''
+class Post_question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.text, nullable=False)
+    user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Post_answer('{self.title}', '{self.date_posted}')"
+'''
+
+'''class Post_answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.text, nullable=False)
+    user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Post_answer('{self.title}', '{self.date_posted}')"
+'''
+
+
+
 
 posts = [
     {
     'author': 'Kelvin Beno',
-    'title': 'Blog post 1',
-    'content':'First post content',
+    'title': 'Question 1',
+    'content':'What is a variable?',
     'date_posted':'January 26, 2019'
     },
     {
 
     'author': 'Everlyn Kihara',
-    'title': 'Blog post 2',
-    'content':'Second post content',
+    'title': 'Question 2',
+    'content':'Is Javascript linked to Java?',
     'date_posted':'January 27, 2019'
     }
 ]
 
-@app.route("/")
+
+@app.route('/')
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 
+            'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
--- forms.py
+++ forms.py
@@ -1,5 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), Email()])
+    submit = SubmitField('Login')
